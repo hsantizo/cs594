a4f9dd2d6052da2758d7405773540212e895280e
hsantizo <henry.santizo@gmail.com>
2015-05-08 14:05:17 -0700

    committing MyFlickrApp

---
+++ MyFlickrApp/app/src/main/java/com/hensan/myflickrapp/Constants.java
@@ -0,0 +1,11 @@
+package com.hensan.myflickrapp;
+
+public class Constants {
+    public static final int NUM_PHOTOS = 10;
+    public static final String TAG = "my code";
+    public static final String API_KEY = "566d73bb6619f8c192b9c4c0249a89c6";
+    public static final String SECRET = "50ba7a23b3816dd8";
+    public static final String LATITUDE = "19.896766";
+    public static final String LONGITUDE = "-155.582782";
+    public static final String RADIUS = "1";
+}
